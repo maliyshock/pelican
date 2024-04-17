45fc4a0f8e84d4e3338175d83adb07f02174a8ea
Denis Maliyshock <maliyshator@gmail.com>
2024-04-17 13:26:36 +0200

    fix type name

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,12 @@
 import { Node } from "reactflow";
-import { BASIC, COMMON, FOREST, LEGENDARY, POOP, RARE, REALLY_RARE, ROCK, STONE, TREE, UNIQUE, WOOD } from "~/constants/dictionary.ts";
+import { BASIC, COMMON, FOREST, LEGENDARY, POOP, RARE, REALLY_RARE, STONE, TREE, UNIQUE, WOOD } from "~/constants/dictionary.ts";
 
 export type Action = "collect" | "explore" | "harvest" | "eat";
 export type ObjectType = "player" | "character" | "object" | "creature" | "region" | "resource" | "food" | "resourceDeposit";
 
 export type Rarity = typeof BASIC | typeof COMMON | typeof UNIQUE | typeof RARE | typeof REALLY_RARE | typeof LEGENDARY;
 export type Region = typeof FOREST;
-export type ResourceDeposit = typeof TREE | typeof ROCK;
+export type ResourceDeposit = typeof TREE | typeof STONE;
 export type Resource = typeof WOOD | typeof STONE | typeof POOP;
 export type ObjectKeyName = Region | ResourceDeposit | Resource | ObjectType;
 
